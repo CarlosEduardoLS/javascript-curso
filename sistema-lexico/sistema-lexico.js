1ba6d7290f57a12a01ea45724f03ad5b35f11a3f
Carlos Eduardo de Lima Salvador <carlos.eduardo@ceresinvestimentos.com>
2021-04-30 17:24:45 -0300

    FEAT: Implementação de exemplos do sistema léxico.

---
+++ sistema-lexico/sistema-lexico.js
@@ -0,0 +1,9 @@
+// Palavras reservadas do javascript
+
+/*
+break, case, catch, class, const, continue, debugger, default, delete, do, else, expert, extends
+finally, for, function, if, import, in, instanceof, new, return, super, switch, this, throw, try
+typeof, var, void, while, with, yield, enum, await, implements, interface, let, package, private
+protected public, static, abstract, boolean, byte, char, double, final, float, goto, int, long
+native, short, synchroniz, ed, throws, transient, volatile
+*/
